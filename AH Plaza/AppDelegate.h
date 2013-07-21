bbbc277f549f704d1f536f4037bb26dc06be7ba1
CasperCL <casperjeekhof@gmail.com>
2013-07-21 11:35:49 +0200

    Initial Commit

---
+++ AH Plaza/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AH Plaza
+//
+//  Created by Casper Eekhof on 21-07-13.
+//  Copyright (c) 2013 JTC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
