b786193f3e2d6f94e3fdf9cddacf92c29999dc1d
CasperCL <casperjeekhof@gmail.com>
2013-08-02 13:43:06 +0200

    Changed transition style to scrolling. Concered switching between working weeks in the 'Planning'

--- AH Plaza/RootViewController.m
+++ AH Plaza/RootViewController.m
@@ -27,7 +27,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     // Configure the page view controller and add it as a child view controller.
-    self.pageViewController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStylePageCurl navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
+    self.pageViewController = [[UIPageViewController alloc] initWithTransitionStyle: UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
     self.pageViewController.delegate = self;
     
     // Download the data and fill the data
