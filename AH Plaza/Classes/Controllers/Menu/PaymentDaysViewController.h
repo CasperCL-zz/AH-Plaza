0101097361375a31b59166907f137f23bb68460d
CasperCL <casperjeekhof@gmail.com>
2013-08-07 17:32:42 +0200

    Payment day listing completed. Closes #10 and closes #11

--- AH Plaza/Classes/Controllers/Menu/PaymentDaysViewController.h
+++ AH Plaza/Classes/Controllers/Menu/PaymentDaysViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface PaymentDaysViewController : UIViewController
+@interface PaymentDaysViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UIView *upcommingPaymentView;
+@property (weak, nonatomic) IBOutlet UILabel *nextPaymentLabel;
 @property (weak, nonatomic) IBOutlet UITableView *upcommingPaymentsDaysTable;
 
 @end
--- AH Plaza/Classes/Controllers/Menu/PaymentDaysViewController.m
+++ AH Plaza/Classes/Controllers/Menu/PaymentDaysViewController.m
@@ -11,9 +11,12 @@
 
 @interface PaymentDaysViewController ()
 
+@property NSMutableArray * tableText;
+
 @end
 
 @implementation PaymentDaysViewController
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,10 +31,78 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    _tableText = [[NSMutableArray alloc] init];
     
     
     _upcommingPaymentView.layer.cornerRadius = 10;
     _upcommingPaymentView.layer.masksToBounds = YES;
+    
+    
+    
+    NSDate * originalDate;
+    NSCalendar *cal = [NSCalendar currentCalendar];
+    
+    
+    // Create file manager
+    NSString *documentsDirectory = [NSHomeDirectory()
+                                    stringByAppendingPathComponent:@"Documents"];
+    NSString *fileLocation = [[NSString alloc] initWithFormat: @"%@/%@", documentsDirectory , @"date.ahpu"];
+    
+    NSDateComponents* dateComponents = [[NSDateComponents alloc]init];
+    if(![[NSFileManager defaultManager] fileExistsAtPath: fileLocation]) {
+        [dateComponents setHour: 14];
+        [dateComponents setDay: 22];
+        [dateComponents setMonth: 7];
+        [dateComponents setYear: 2013];
+        NSDate *dateToWrite = [cal dateFromComponents:dateComponents];
+        [NSKeyedArchiver archiveRootObject: dateToWrite toFile: fileLocation];
+        NSLog(@"Wrote: %@", dateToWrite);
+        originalDate = dateToWrite;
+    } else  {
+        NSDate *savedDate = [NSKeyedUnarchiver unarchiveObjectWithFile: fileLocation];
+        NSLog(@"Read: %@", savedDate);
+        originalDate = savedDate;
+    }
+    
+    // Check if outdated
+    NSDate * now = [[NSDate alloc] init];
+    BOOL outdated = [originalDate compare: now] == NSOrderedAscending;
+    if(outdated){ // if outdated go to new date
+        while ([originalDate compare: now] == NSOrderedAscending) {
+            NSLog(@"date is outdated");
+            dateComponents = [[NSDateComponents alloc]init];
+            [dateComponents setDay: 28];
+            NSCalendar* calendar = [NSCalendar currentCalendar];
+            originalDate = [calendar dateByAddingComponents: dateComponents toDate: originalDate options:0];
+        }
+        // Write away the correct date for the next time
+        [NSKeyedArchiver archiveRootObject: originalDate toFile: fileLocation];
+    }
+    
+    
+    
+    // Generate the upcomming 9 months
+    for (int i = 0; i < 9; i++) {
+        NSDateFormatter * df = [[NSDateFormatter alloc] init];
+        [df setDateFormat:@"dd MMMM"];
+        NSString *newDateS = [df stringFromDate: originalDate];
+        
+        if(i != 0)
+            [_tableText addObject: newDateS];
+        else
+            [_nextPaymentLabel setText: newDateS];
+        
+        dateComponents = [[NSDateComponents alloc]init];
+        [dateComponents setDay: 28];
+        NSCalendar* calendar = [NSCalendar currentCalendar];
+        NSDate* newDate = [calendar dateByAddingComponents: dateComponents toDate: originalDate options:0];
+        
+        originalDate = newDate;
+        
+
+        NSLog(@"New Date: %@", newDateS);
+    }
+    [_upcommingPaymentsDaysTable reloadData];
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,6 +114,35 @@
 - (void)viewDidUnload {
     [self setUpcommingPaymentView:nil];
     [self setUpcommingPaymentsDaysTable:nil];
+    [self setNextPaymentLabel:nil];
     [super viewDidUnload];
 }
+
+
+
+#pragma UITableView delegates
+-(void) tableView:(UITableView *) tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+}
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_tableText count];
+}
+
+-(UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    if (cell == nil){
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    UIView *bgColorView = [[UIView alloc] init];
+    [bgColorView setBackgroundColor:[UIColor blackColor]];
+    cell.textLabel.text = [_tableText objectAtIndex:indexPath.row];
+    cell.textLabel.font = [UIFont fontWithName:@"STHeitiTC-Light" size: 17];
+    cell.textLabel.backgroundColor = [UIColor clearColor];
+    return cell;
+}
+
+
 @end
